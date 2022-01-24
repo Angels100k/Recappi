134559d7597939782dc07db9beaaa44c09eb0c0e
VeraLynnAmerongen <59731177+VeraLynnAmerongen@users.noreply.github.com>
2022-01-24 13:49:17 +0100

    admin start

---
+++ webpage/adminmain.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?=dd_head("homepage")?>
+</head>
+
+<body style="background-color: var(--background)">
+<?php require $dir.'/elements/navbar/navbar.php';?>
+    <div id="main-top" class="main-top row">
+        <div class="col text-center">
+        <button id="Btnfriends" class="list-main list-main-active button-no-style">
+        My friends
+        </button>    
+        </div>
+        <div class="col text-center">
+        <button id="BtnDiscover" class="list-main button-no-style">
+        Discover
+        </button>    
+        </div>
+    </div>
+    <div id="main-body" class="main-body homepage-container row flex-wrap-no">
+        <div class="main-container pagefriends">
+            <h2>following</h2>
+            <div class="row">
+                <?php 
+                    $stmt = $sqlQuery->getfriends();
+                    while($row = $stmt->fetch()):
+                        echo dd_layout_friend($row);
+                    endwhile;
+                    ?>
+            </div>
+        </div>
+        <div class="main-container pagediscover mb--4">
+            <h2>Recipe feed</h2>
+            <div class="row mb-4">
+                <?php 
+                $stmt = $sqlQuery->getcookbookdiscover();
+                while($row = $stmt->fetch()):
+                        echo dd_layout_post($row['id'], $row["recipe"], $row["preptime"],$row["difficulty"], $row["likes"], $row["repsonses"], $row["image"], $row["type"], $row["likeid"], $row["saveid"], $row["userid"]);
+                    endwhile;
+                ?>
+            </div>
+        </div>
+    </div>
+    <?php require $dir.'/elements/main-footer.php';?>
+
+    <script src="/assets/js/main.js"></script>
+</body>
+
+</html>
